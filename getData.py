37e01a5164d45792a6c702978595fee1ea31840c
Eric Kennedy <ericbkennedy@gmail.com>
2022-11-04 11:11:30 -0700

    Only formatting changes from Black

--- getData.py
+++ getData.py
@@ -8,43 +8,43 @@ import csv
 import requests
 import sys
 
-MISSING_KEY_MSG = 'Create a file .apikey.txt with your own key from https://www.alphavantage.co/support/#api-key'
-apiKeyFilename = '.apikey.txt'
-apiKey = 'demo'
-ticker = ''
+MISSING_KEY_MSG = "Create a file .apikey.txt with your own key from https://www.alphavantage.co/support/#api-key"
+apiKeyFilename = ".apikey.txt"
+apiKey = "demo"
+ticker = ""
 
 try:
-    with open(apiKeyFilename, 'r') as keyFile:
+    with open(apiKeyFilename, "r") as keyFile:
         apiKey = keyFile.readline()
 except Exception as e:
     print(MISSING_KEY_MSG)
     exit()
 
 if len(sys.argv) < 2:
-    print('Enter ticker e.g.: python3 getData.py SPY')
+    print("Enter ticker e.g.: python3 getData.py SPY")
     exit()
 else:
     ticker = sys.argv[1]
 
-url = f'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol={ticker}&outputsize=full&datatype=csv&apikey={apiKey}'
+url = f"https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol={ticker}&outputsize=full&datatype=csv&apikey={apiKey}"
 
 r = requests.get(url)
 
-if len(r.text) < 100 or 'Error' in r.text:
-    print(f'Ticker {ticker} may require a class (e.g. BRK-A) because response length was only {len(r.text)}')
+if len(r.text) < 100 or "Error" in r.text:
+    print("Ticker may require a class (e.g. BRK-A) because response length was:")
     print(r.text)
     exit()
 
-output = [] # list to hold columns after removing high, low and volume
+output = []  # list to hold columns after removing high, low and volume
 
 csvInput = r.text.splitlines()
 
 for line in csvInput:
-    row = line.split(',')
-    if len(row) == 1: # allow trailing empty line
+    row = line.split(",")
+    if len(row) == 1:  # allow trailing empty line
         continue
     elif len(row) != 8:
-        print(f'Unexpected row length {len(row)} in {row}')
+        print(f"Unexpected row length {len(row)} in {row}")
         exit()
     closingDate = row[0]
     openPrice = row[1]
@@ -53,21 +53,25 @@ for line in csvInput:
     closePrice = row[4]
     adjClose = row[5]
 
-    if closePrice != 'close' and (float(closePrice) / float(adjClose) > 1.9 or float(closePrice) / float(adjClose) < 0.5):
-        print(f'A stock split may have occurred on {closingDate} because the adjusted close {adjClose} != historic {closePrice}')
-        print('trendFollowing.py is only intended to backtest ETFs which do not have stock splits')
+    if closePrice != "close" and (
+        float(closePrice) / float(adjClose) > 1.9
+        or float(closePrice) / float(adjClose) < 0.5
+    ):
+        print(
+            f"A stock split may have occurred on {closingDate} because adjusted close {adjClose} != historic {closePrice}"
+        )
+        print("This is only intended to backtest ETFs which do not have stock splits")
         exit()
 
     # skip volume in row[6]
     divAmount = row[7]
     output.append((closingDate, openPrice, closePrice, adjClose, divAmount))
 
-outputFilename = f'{ticker}-monthly.csv'
+outputFilename = f"{ticker}-monthly.csv"
 
-with open(outputFilename, mode='w', newline='') as csvOutput:
-    csvWriter = csv.writer(csvOutput, delimiter=',', quotechar="'")
+with open(outputFilename, mode="w", newline="") as csvOutput:
+    csvWriter = csv.writer(csvOutput, delimiter=",", quotechar="'")
     for row in output:
         csvWriter.writerow(row)
 
-print(f'Output saved to {outputFilename}')
-
+print(f"Output saved to {outputFilename}")
--- trendFollowing.py
+++ trendFollowing.py
@@ -21,29 +21,29 @@ INITAL_BALANCE = 10000.0
 isTaxableAccount = False
 
 if len(sys.argv) < 2:
-    raise Exception('Enter ticker (and optionally taxable)')
+    raise Exception("Enter ticker (and optionally taxable)")
 else:
     ticker = sys.argv[1]
-    if len(sys.argv) == 3 and 'taxable' == sys.argv[2]:
+    if len(sys.argv) == 3 and "taxable" == sys.argv[2]:
         isTaxableAccount = True
 
 months, output = [], []
 
-with open(f'{ticker}-monthly.csv', newline='') as csvInput:
-    reader = csv.reader(csvInput, delimiter=',', quotechar='|')
+with open(f"{ticker}-monthly.csv", newline="") as csvInput:
+    reader = csv.reader(csvInput, delimiter=",", quotechar="|")
     for row in reader:
         if len(row) == 5:
             closingDate = row[0]
-            if closingDate != 'timestamp': # skip header
-                openPrice = float(row[1]) # don't call it open to avoid redefining open()
+            if closingDate != "timestamp":  # skip header
+                openPrice = float(row[1])
                 closePrice = float(row[2])
                 # adjClose = float(row[3]) # unused since it double counts dividend adjustments
                 dividend = float(row[4])
                 monthlyData = (closingDate, openPrice, closePrice, dividend)
-                months.insert(0, monthlyData) # insert older months at start for ascending order
+                months.insert(0, monthlyData)  # insert older months at start
         else:
-            print('Check CSV format', *row)
-            raise ValueError('Invalid row length', len(row))
+            print("Check CSV format", *row)
+            raise ValueError("Invalid row length", len(row))
 
 last10closes = []
 rollingSum = movingAverage = shares = bhShares = 0.0
@@ -52,54 +52,56 @@ cash = costBasis = INITAL_BALANCE
 
 # SPY was above the 10 month MA at the end of October 1998 (110 close v 106.66 MA) so start both long
 initialPrice = months[0][1]
-trendFollowingOutperformingDate = months[0][0] # Used for 'Trend following outperformed through {date}'
+trendFollowingOutperformingDate = months[0][0]  # track if/when buy-and-hold outperforms
 shares = round(cash / initialPrice, 2)
-cash = 0.0 # since we start out long
+cash = 0.0  # since we start out long
 bhShares = shares
 
 for index, (closingDate, openPrice, closePrice, dividend) in enumerate(months):
-    last10closes.append( (closingDate, openPrice, closePrice, dividend) )
+    last10closes.append((closingDate, openPrice, closePrice, dividend))
     rollingSum += closePrice
     taxes = 0
-    comments = ''
+    comments = ""
 
     if index + 1 < len(months):
         nextOpen = months[index + 1][1]
     else:
-        nextOpen = closePrice # to calculate buying or selling for the final month
+        nextOpen = closePrice  # to calculate buying or selling for the final month
 
-    if dividend > 0.0: # reinvest dividend if long
+    if dividend > 0.0:  # reinvest dividend if long
         if isTaxableAccount:
-            dividend *= (1 - DIVIDEND_TAX_RATE) # reduce by tax rate
+            dividend *= 1 - DIVIDEND_TAX_RATE  # reduce by tax rate
 
         bhShares += round(bhShares * dividend / closePrice, 2)
 
-        if shares > 0.0: # will only get the dividend if long but could still sell based on MA
+        if shares > 0.0:  # will get the dividend if long but could sell based on MA
             cash += round(shares * dividend, 2)
 
-    if len(last10closes) > 10: # drop entry from 11 months ago
+    if len(last10closes) > 10:  # drop entry from 11 months ago
         droppedMonth = last10closes.pop(0)
-        rollingSum -= droppedMonth[1] # closing price is index = 1
+        rollingSum -= droppedMonth[1]  # closing price is index = 1
 
     if len(last10closes) == 10:
-        movingAverage = round(rollingSum/10, 2)
+        movingAverage = round(rollingSum / 10, 2)
         if closePrice >= movingAverage:
             if shares == 0.0:
                 shares = round(cash / nextOpen, 2)
-                comments = f'Bought {shares} at {nextOpen}'
+                comments = f"Bought {shares} at {nextOpen}"
                 cash = 0.0
-        else: # close below MA so sell if long or stay in cash
+        else:  # close below MA so sell if long or stay in cash
             if shares > 0.0:
                 cash += round(shares * nextOpen, 2)
-                comments = f'Sold {round(shares, 2)} at {nextOpen}'
+                comments = f"Sold {round(shares, 2)} at {nextOpen}"
                 if isTaxableAccount and cash > costBasis:
-                    taxes = round((cash - costBasis) * CAPITAL_GAIN_RATE, 2) # deduct taxes
-                    comments += f' and paid {round(taxes)} on gain {round(cash - costBasis)}'
+                    taxes = round((cash - costBasis) * CAPITAL_GAIN_RATE, 2)
+                    gain = round(cash - costBasis)
+                    comments += (f" and paid {round(taxes)} on gain {gain}")
                     cash -= taxes
                     costBasis = cash
                 shares = 0
 
-    if len(last10closes) < 10 or shares > 0.0: # reinvest dividend until 10 month MA can be calculated
+    if (len(last10closes) < 10 or shares > 0.0):
+        # reinvest dividend until 10 month MA can be calculated
         shares += round(shares * dividend / closePrice, 2)
         cash = 0.0
 
@@ -108,42 +110,52 @@ for index, (closingDate, openPrice, closePrice, dividend) in enumerate(months):
     if tfValue > bhValue:
         trendFollowingOutperformingDate = closingDate
 
-    output.append((closingDate,
-                   openPrice,
-                   closePrice,
-                   movingAverage,
-                   bhShares,
-                   '${:,}'.format(bhValue),
-                   shares,
-                   '${:,}'.format(tfValue),
-                   taxes,
-                   comments))
+    output.append(
+        (
+            closingDate,
+            openPrice,
+            closePrice,
+            movingAverage,
+            bhShares,
+            "${:,}".format(bhValue),
+            shares,
+            "${:,}".format(tfValue),
+            taxes,
+            comments,
+        )
+    )
 
 buyHoldReturn = round(100 * (closePrice * bhShares) / INITAL_BALANCE)
 trendFollowingReturn = round(100 * (closePrice * shares + cash) / INITAL_BALANCE)
 
-print(f'{ticker} buy and hold {buyHoldReturn}% vs {trendFollowingReturn}% for trend following since {initialMonth}')
+print(
+    f"{ticker} buy and hold {buyHoldReturn}% vs {trendFollowingReturn}% for trend following since {initialMonth}"
+)
 
 if trendFollowingOutperformingDate == initialMonth:
-    print('Trend following never outperformed buy-and-hold')
+    print("Trend following never outperformed buy-and-hold")
 else:
-    print(f'Trend following outperformed through {trendFollowingOutperformingDate}')
+    print(f"Trend following outperformed through {trendFollowingOutperformingDate}")
 
-filename = ticker + '-return'
+filename = ticker + "-return"
 if isTaxableAccount:
-    filename += '-taxable'
+    filename += "-taxable"
 
-with open(f'{filename}.csv', mode='w', newline='') as csvOutput:
-    csvWriter = csv.writer(csvOutput, delimiter=',', quotechar='"')
-    csvWriter.writerow(['Month end',
-                        'Open at start of month',
-                        'Close at end of month',
-                        '10 month moving average',
-                        'Buy and hold shares',
-                        'Buy and hold value',
-                        'Trend following shares',
-                        'Trend following value',
-                        'Taxes paid',
-                        'Comments']);
+with open(f"{filename}.csv", mode="w", newline="") as csvOutput:
+    csvWriter = csv.writer(csvOutput, delimiter=",", quotechar='"')
+    csvWriter.writerow(
+        [
+            "Month end",
+            "Open at start of month",
+            "Close at end of month",
+            "10 month moving average",
+            "Buy and hold shares",
+            "Buy and hold value",
+            "Trend following shares",
+            "Trend following value",
+            "Taxes paid",
+            "Comments",
+        ]
+    )
     for row in output:
         csvWriter.writerow(row)
